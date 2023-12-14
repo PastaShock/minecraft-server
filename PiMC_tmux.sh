6805dcd679a45bfdb3b9e64d08d3982c5f14ba96
George Pastushok <gpastushok@yahoo.com>
2023-12-14 00:42:03 +0000

    another mid-day update; updated PiMC_tmux to cd into server dir, then launch server. Also added vertical split with htop on the bottom pane to monitor vitals

--- PiMC_tmux.sh
+++ PiMC_tmux.sh
@@ -9,7 +9,11 @@ if [ "$EXISTS" = "" ] #tmux session exists:
 then
 	tmux new-session -d -s $SESSION
 	tmux rename-window -t 0 'PiMC Console'
-	tmux send-keys -t $SESSION:0 "sh $(MCSERV)mcgang.sh" C-m
+  tmux send-keys -t $SESSION:0 "cd $MCSERV" C-m
+	tmux send-keys -t $SESSION:0 "sh mcgang.sh" C-m
+  tmux split-window -v
+  tmux select-pane 1
+  tmux send-keys "htop" C-m
 fi
 
 tmux attach-session -t $SESSION:0
