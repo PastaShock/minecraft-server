1a8a3df1aa53feb177281e5372161c32ba1df27a
George Pastushok <gpastushok@yahoo.com>
2024-08-19 04:55:52 +0000

    Daily update 2024-08-18

--- mcgang.sh
+++ mcgang.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 SCRIPTPATH="$(pwd)/$(dirname $0)/minecraft_server.1.12.jar"
+# ENV:
+source .env
 while true
 do
+  .restart-network.sh
 	java -jar "minecraft_server.1.12.jar"
 	sleep 10
 done
