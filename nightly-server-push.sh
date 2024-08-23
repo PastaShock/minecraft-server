0584da900213abf3f14efd48673a0a49df8162a8
George Pastushok <gpastushok@yahoo.com>
2024-08-22 21:58:20 -0700

    manual backup

--- nightly-server-push.sh
+++ nightly-server-push.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+# - Update the git storage of the project -
+#
+# Declare all necessary variables
+# or import them from an env file
+# source .env
+# 
 ME=$(basename "$0")
 DATE=$(date)
 STRING="Nightly automated server backup of player and world data - $DATE"
+
 git pull
 git add -A
 git commit -m "$STRING"
